61f2472194b33403f0dbdc3ccb4a6ba4a006156b
Harvey <harveystedman@gmail.com>
2021-03-22 11:39:52 +0000

    Simplified rtsp republish method

--- src/rtspSetup.py
+++ src/rtspSetup.py
@@ -13,7 +13,7 @@ class bashSetUp():
         while not rospy.is_shutdown():  
             if (self.serverStarted==False):
                 print("Starting Stream")
-                bashCommand = '~/catkin_ws/src/ros_rtsp/src/rtsp_server "rtspsrc location={rtsp_uri} ! queue2 ! rtph264depay ! h264parse ! avdec_h264 ! videoconvert ! x264enc pass=0 quantizer={qvalue} speed-preset={speed} bitrate={bitrate} tune=zerolatency sliced-threads=true ! rtph264pay name=pay0 pt=96" {port} {url}'.format(rtsp_uri=str(rospy.get_param("rtsp_uri")), framerate=str(rospy.get_param("fps")), qvalue=str(rospy.get_param("qvalue")), speed=str(rospy.get_param("speed-preset")), bitrate=str(rospy.get_param("bitrate")), port=str(rospy.get_param("port")), url=str(rospy.get_param("url")))
+                bashCommand = '~/catkin_ws/src/ros_rtsp/src/rtsp_server "rtspsrc location={rtsp_uri} ! queue2 ! rtph264depay ! rtph264pay name=pay0 pt=96" {port} {url}'.format(rtsp_uri=str(rospy.get_param("rtsp_uri")), framerate=str(rospy.get_param("fps")), qvalue=str(rospy.get_param("qvalue")), speed=str(rospy.get_param("speed-preset")), bitrate=str(rospy.get_param("bitrate")), port=str(rospy.get_param("port")), url=str(rospy.get_param("url")))
                 process = subprocess.Popen(bashCommand, shell=True)
                 self.serverStarted = True
                 
